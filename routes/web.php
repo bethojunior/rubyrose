78de0a496dc7338ce6230975c74aaa90af6d618b
bethojunior <hgomescostajr@gmail.com>
2020-11-23 15:40:15 -0300

    teest

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,15 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+Auth::routes();
 
 Route::get('/', function () {
     return view('auth.login');
 });
+Route::get('/login', function () {
+    return view('auth.login');
+});
 
-Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/home', 'HomeController@index')->name('home');
