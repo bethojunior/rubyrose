92e97df5128e6f2a62b5fa076b9d4799b48fade6
bethojunior <hgomescostajr@gmail.com>
2020-11-27 12:10:26 -0300

    add color

--- public/js/modules/products/list.js
+++ public/js/modules/products/list.js
@@ -1,12 +1,3 @@
 $('.carousel').carousel({
     interval: 2000
 })
-
-elementProperty.addEventInElement('.disable-product','onclick',function (){
-    SwalCustom.dialogConfirm('Deseja desabilitar esse produto?','Ele ficará indisponivel para seus revendedores',status => {
-        if(!status)
-            return false;
-
-        alert('ok')
-    })
-})
