d03cf3c81cee922c8358f6d670ca4998ebc9bdcf
betho <hgomescostajr@gmail.com>
2021-01-29 15:37:19 -0300

    ok

--- public/js/modules/products/edit.js
+++ public/js/modules/products/edit.js
@@ -5,7 +5,10 @@ elementProperty.addEventInElement('.delete-image','onclick',function (){
     let id = this.getAttribute('id');
     SwalCustom.dialogConfirm('Deseja apagar essa imagem?','',function (){
         ProductController.deleteImage(id).then(response => {
-            console.log(response)
+            if(response.status)
+                return swal('Erro ao deletar imagem','Contate o suporte','error');
+
+            return swal('Imagem excluida com sucesso','','success');
         })
     })
 })
